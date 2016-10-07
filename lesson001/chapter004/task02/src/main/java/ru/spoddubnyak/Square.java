09e68d21e4d4ebcdf970d7b8f653dc31aaac3249
Sergei <poddubnyak@iac.spb.ru>
2016-10-07 17:08:31 +0300

    job lesson1-chapter4-task2

---
+++ lesson001/chapter004/task02/src/main/java/ru/spoddubnyak/Square.java
@@ -0,0 +1,23 @@
+package ru.spoddubnyak;
+
+public class Square {
+  private float a;
+  private float b;
+  private float c;
+  
+   public Square(float a, float b, float c) {
+      this.a = a;
+	  this.b = b;
+	  this.c = c;
+}
+
+  public float calculate (int x) {
+	  return this.a*x*x + this.b*x + this.c;
+  }	  
+  
+  public void show(int start, int finish, int step) {
+	  for (int i = start; i <= finish; i +=step) {
+		  System.out.println(calculate(i));
+	  }
+  }
+}
---
+++ lesson001/chapter004/task02/src/test/java/ru/spoddubnyak/SquareTest.java
@@ -0,0 +1,20 @@
+package ru.spoddubnyak;
+
+  import org.junit.Test;
+  import static org.hamcrest.core.Is.is;
+  import static org.junit.Assert.*;
+
+public class SquareTest {
+  
+  @Test
+    public void whenPositiveNumberThenCalculationSquare() {
+       Square square = new Square(3,6,9);
+	   assertThat(square.calculate(2), is(33F));
+	}  
+	
+	@Test
+    public void whenNegativeNumberThenCalculationSquare() {
+       Square square = new Square(-1,-1,-1);
+	   assertThat(square.calculate(2), is(-7F));
+	}  
+}
