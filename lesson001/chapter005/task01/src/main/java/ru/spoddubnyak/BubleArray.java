5834e937638ab01d97fcab1249e3df2281ba7b85
Sergei <poddubnyak@iac.spb.ru>
2016-10-10 14:35:22 +0300

    The job lesson1-chapter5-task01

---
+++ lesson001/chapter005/task01/src/main/java/ru/spoddubnyak/BubleArray.java
@@ -0,0 +1,23 @@
+	package ru.spoddubnyak;
+
+  public class BubleArray {
+    public int[] values;
+   
+   public BubleArray(int[] values){
+     this.values = values;   
+   }
+  
+	public int[] sortBuble () {
+		for (int i = 0; i < this.values.length-1; i++) {
+			for (int j = 0; j < this.values.length-1-i; j++) {
+				if (this.values[j] > this.values[j+1]) {
+					int temp = this.values[j];
+					this.values[j] = this.values[j+1];
+					this.values[j+1] = temp;				
+				}
+			}
+			
+		}
+	return this.values;	
+	}  
+  }
---
+++ lesson001/chapter005/task01/src/test/java/ru/spoddubnyak/BubleArrayTest.java
@@ -0,0 +1,26 @@
+package ru.spoddubnyak;
+
+  import org.junit.Test;
+  import static org.hamcrest.core.Is.is;
+  import static org.junit.Assert.*;
+
+public class BubleArrayTest {
+  
+  @Test
+    public void whenSortArrayBuble() {
+       BubleArray array = new BubleArray(new int[] {4,7,4,0,3,1,6,9});
+	   assertThat(array.sortBuble(), is(new int[] {0,1,3,4,4,6,7,9}));
+	}  
+	
+	@Test
+    public void whenCreationEmptyArray() {
+       BubleArray array = new BubleArray(new int[] {});
+	   assertThat(array.sortBuble(), is(new int[] {}));
+	} 
+	
+	@Test
+    public void whenNotSorting() {
+       BubleArray array = new BubleArray(new int[] {1,2,3,4,5,6,7});
+	   assertThat(array.sortBuble(), is(new int[] {1,2,3,4,5,6,7}));
+	} 
+}
