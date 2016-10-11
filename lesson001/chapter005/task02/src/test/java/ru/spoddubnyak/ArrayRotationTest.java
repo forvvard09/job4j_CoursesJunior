e59daf620b8c5284c2d3d04ea74e1785c5cfa21d
Sergei <poddubnyak@iac.spb.ru>
2016-10-11 12:48:12 +0300

    The job lesson1-chapter5-task02

---
+++ lesson001/chapter005/task02/src/test/java/ru/spoddubnyak/ArrayRotationTest.java
@@ -0,0 +1,41 @@
+package ru.spoddubnyak;
+
+  import org.junit.Test;
+  import static org.hamcrest.core.Is.is;
+  import static org.junit.Assert.*;
+
+public class ArrayRotationTest {
+	
+  ArrayRotation sourceArray = new ArrayRotation  (new int[][] { 
+                                                     		        {1,1,1,1,1},
+	                                                                {2,2,2,2,2},
+               		                                                {3,3,3,3,3},
+																	{4,4,4,4,4},
+																	{5,5,5,5,5},
+	                                                                });
+  
+  @Test
+    public void whenTurnArrayRight() {															   
+		int[][] checkedArray = { 
+			                    {5,4,3,2,1},
+							    {5,4,3,2,1},
+								{5,4,3,2,1},
+								{5,4,3,2,1},
+								{5,4,3,2,1}					
+							   };
+		
+       assertThat(sourceArray.turnArrayRight(), is(checkedArray));
+    }  	
+	
+	 @Test
+    public void whenTurnArrayLeft() {															   
+		int[][] checkedArray = { 
+			                    {1,2,3,4,5},
+							    {1,2,3,4,5},
+								{1,2,3,4,5},
+								{1,2,3,4,5},
+								{1,2,3,4,5},
+							   };
+       assertThat(sourceArray.turnArrayLeft(), is(checkedArray));
+    }  	
+}
