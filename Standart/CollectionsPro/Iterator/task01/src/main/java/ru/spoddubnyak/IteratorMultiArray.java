f2dbab8286baf2830ab5bdff03e6d8f5bee0cb31
forvvard09 <forvvard09@gmail.com>
2017-04-24 19:02:09 +0300

    Done task01. Theme Collections.Pro. Iterator.

--- Standart/CollectionsPro/Iterator/task01/src/main/java/ru/spoddubnyak/IteratorMultiArray.java
+++ Standart/CollectionsPro/Iterator/task01/src/main/java/ru/spoddubnyak/IteratorMultiArray.java
@@ -7,30 +7,46 @@ import java.util.Iterator;
  *
  * @author Sergei Poddubnyak (forvvard09@gmail.com)
  * @version 1.0
- * @since 21.04.2017
+ * @since 24.04.2017
  */
 public class IteratorMultiArray implements Iterator {
 
+    /**
+     * property values - array with input parameters for iteration.
+     */
     private final int[][] values;
-    private int index = 0;
 
+    /**
+     * property column - column in values.
+     */
+    private int column = 0;
+
+    /**
+     * property row - row in values.
+     */
+    private int row = 0;
+
+    /**
+     * Constructor it creates a new object IteratorMultiArray with the specified values.
+     *
+     * @param values - set property value
+     */
     public IteratorMultiArray(final int[][] values) {
-        this.values = values;
-
+        int[][] copyValues = values.clone();
+        this.values = copyValues;
     }
 
     @Override
     public boolean hasNext() {
-        return values[values.length].length < index;
+        return this.column < values.length && this.row < values[this.column].length;
     }
 
     @Override
     public Object next() {
-        int result = 0;
-        for (int i = 0; i < values.length; i++) {
-            for(int j = 0; j < values[i].length; j++) {
-                   result = values[i][j];
-            }
+        int result = values[this.column][this.row++];
+        if (this.row == values[this.column].length && this.column < values.length) {
+            this.column++;
+            this.row = 0;
         }
         return result;
     }
