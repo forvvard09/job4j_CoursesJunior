6ac9670f9b9ad7684b035b33f8019ee20c71dac2
Sergei Poddubnyak <poddubnyak@iac.spb.ru>
2017-07-12 01:11:01 +0300

    Done SimpleLinledList. Theme Cillections Pro. List.

---
+++ Start/CollectionsLite/testTwoTaskCollectionsLite/src/main/java/ru/spoddubnyak/FormingDirectoryUnits.java
@@ -0,0 +1,24 @@
+package ru.spoddubnyak;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ *
+ * @author Sergei Poddubnyak (forvvard09@gmail.com)
+ * @version 1.0
+ * @since 04.07.2017
+ */
+public class FormingDirectoryUnits {
+
+    public static void main(String[] args) {
+        List<String> directoryUnits = new ArrayList<String>();
+        directoryUnits.addAll(Arrays.asList("K1\\SK1","K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"));
+
+    }
+
+
+
+}
