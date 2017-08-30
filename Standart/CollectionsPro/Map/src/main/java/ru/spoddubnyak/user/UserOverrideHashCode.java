0eb8ba34a90b8c5291a7e07cd637348a8681aa2c
Sergei Poddubnyak <poddubnyak@iac.spb.ru>
2017-08-30 12:37:13 +0300

    Update system project.

---
+++ Standart/CollectionsPro/Map/src/main/java/ru/spoddubnyak/user/UserOverrideHashCode.java
@@ -0,0 +1,39 @@
+package ru.spoddubnyak.user;
+
+import java.util.Calendar;
+
+/**
+ * Class User for learning purposes, override method hashCode().
+ *
+ * @author Sergei Poddubnyak (forvvard09@gmail.com)
+ * @version 1.0
+ * @since 29.08.2017
+ */
+public class UserOverrideHashCode extends User {
+    /**
+     * Constructor it creates a new Object User with the specified values.
+     *
+     * @param name     - name for object User
+     * @param children - number children
+     * @param birthday - birth day
+     */
+    public UserOverrideHashCode(String name, int children, Calendar birthday) {
+        super(name, children, birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        final int koefTwo = 17;
+        final int koef = 31;
+        int result = koefTwo;
+        result = koef * result + (getName() != null ? getName().hashCode() : 0);
+        result = koef * result + getChildren();
+        result = koef * result + (getBirthday() != null ? getBirthday().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
