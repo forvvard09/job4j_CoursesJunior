2b4acccadb297350247942bb8cb789c13babc720
Sergei Poddubnyak <forvvard09@gmail.com>
2018-03-13 20:11:34 +0300

    Multithreading. Wait, Notify, NotifyAll. 3. Реализовать собственный механизм блокировок Lock. [#1100]

---
+++ 2_Standart/02_Multithreading/04_WaitNotiyNotifyAll/task_2_02_04_03/src/main/java/ru/spoddubnyak/SimpleLock.java
@@ -0,0 +1,107 @@
+package ru.spoddubnyak;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Class SimpleLock implement interface Lock.
+ *
+ * @author Sergei Poddubnyak (forvvard09@gmail.com)
+ * @version 1.0
+ * @since 12.03.2018
+ */
+public class SimpleLock implements Lock {
+    /**
+     * property UNIVERSAL_TIMER - universal timer for  .
+     */
+    private static final long UNIVERSAL_TIMER = 333;
+    /**
+     * property Semaphore.
+     */
+    private final Semaphore semaphore;
+    /**
+     * property lock - object free flag.
+     */
+    private boolean lock;
+
+    /**
+     * Constructor it creates a new object ThreadPool with the specified.
+     */
+    public SimpleLock() {
+        this.semaphore = new Semaphore(1);
+        lock = false;
+    }
+
+    /**
+     * Gettter for property lock, return state of object, false - object is free, true - isn't free.
+     *
+     * @return state of object, false - object is free, true - isn't free
+     */
+    public boolean isLock() {
+        return this.lock;
+    }
+
+    @Override
+    public void lock() {
+        while (!this.lock && !Thread.currentThread().isInterrupted()) {
+            try {
+                semaphore.acquire();
+                this.lock = true;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        boolean result = tryLock();
+        if (!Thread.currentThread().isInterrupted() && !result) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
+    public boolean tryLock() {
+        boolean result = false;
+        if (!this.lock && !Thread.currentThread().isInterrupted()) {
+            result = true;
+            this.lock = true;
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        boolean result = false;
+        long timeToEnd = System.currentTimeMillis() + unit.toMillis(time);
+        while (this.lock && timeToEnd - System.currentTimeMillis() >= 0 && !Thread.currentThread().isInterrupted()) {
+            Thread.sleep(UNIVERSAL_TIMER);
+        }
+        if (!this.lock) {
+            result = true;
+            lock();
+        }
+        return result;
+    }
+
+    @Override
+    public void unlock() {
+        if (this.lock && !Thread.currentThread().isInterrupted()) {
+            this.lock = false;
+            semaphore.release();
+        }
+    }
+
+    @Override
+    public Condition newCondition() {
+        throw new UnsupportedOperationException("Expected error. Run method newCondition.");
+    }
+}
---
+++ 2_Standart/02_Multithreading/04_WaitNotiyNotifyAll/task_2_02_04_03/src/test/java/ru/spoddubnyak/SimpleLockTest.java
@@ -0,0 +1,98 @@
+package ru.spoddubnyak;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class SimpleLockTest for testing method's class SimpleLock implements interface Lock.
+ *
+ * @author Sergei Poddubnyak (forvvard09@gmail.com)
+ * @version 1.0
+ * @since 12.03.2018
+ */
+public class SimpleLockTest {
+
+    /**
+     * property Semaphore.
+     */
+    private static final int TIMER_SLEEP = 5000;
+    /**
+     * property Semaphore.
+     */
+    private SimpleLock simpleLock = new SimpleLock();
+
+    /**
+     * Test for methods lock(), tryLock() and unlock().
+     */
+    @Test
+    public void whenLockUnlockObjectThenExpectedsStateStateLock() {
+        assertThat(false, is(simpleLock.isLock()));
+        simpleLock.lock();
+        assertThat(true, is(simpleLock.isLock()));
+        assertThat(false, is(simpleLock.tryLock()));
+        simpleLock.unlock();
+        assertThat(false, is(simpleLock.isLock()));
+        assertThat(true, is(simpleLock.tryLock()));
+
+    }
+
+    /**
+     * Test for methods newCondition().
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenRunMethodThenExpectedError() {
+        simpleLock.lock();
+        simpleLock.newCondition();
+    }
+
+    /**
+     * Test for methods newCondition().
+     */
+    @Test
+    public void whenTryLockTimeThenExpectedStateLock() {
+        simpleLock.lock();
+        try {
+            assertThat(false, is(simpleLock.tryLock(TIMER_SLEEP, TimeUnit.MILLISECONDS)));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        simpleLock.unlock();
+        try {
+            assertThat(true, is(simpleLock.tryLock(TIMER_SLEEP, TimeUnit.MILLISECONDS)));
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test for methods newCondition().
+     */
+    @Test
+    public void whenThen() {
+        assertThat(false, is(simpleLock.isLock()));
+        Thread.currentThread().interrupt();
+        simpleLock.lock();
+        assertThat(false, is(simpleLock.isLock()));
+    }
+
+    /**
+     * Test for methods newCondition().
+     */
+    @Test
+    public void whenThen2() {
+        simpleLock.lock();
+        simpleLock.unlock();
+        try {
+            simpleLock.lockInterruptibly();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertThat(true, is(simpleLock.isLock()));
+        assertThat(false, is(simpleLock.tryLock()));
+    }
+}
